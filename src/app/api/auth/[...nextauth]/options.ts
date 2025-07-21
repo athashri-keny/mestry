11e6cddc5e4c4de6bdbef9572a63ceaf82076274
Athashri Keny <athashrikeny38@gmail.com>
2025-07-21 23:57:34 +0530

    resolving bugs

--- src/app/api/auth/[...nextauth]/options.ts
+++ src/app/api/auth/[...nextauth]/options.ts
@@ -45,7 +45,7 @@ export const authOptions: NextAuthOptions = {
              }
 
         } catch (error) {
-            throw error 
+            console.log("Error" , error)
         }
     }
         })
